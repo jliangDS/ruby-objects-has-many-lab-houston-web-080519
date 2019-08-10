55ddb54b60d9900d62435d057f5fb5c0fa00f7f2
Jun Liang <illuminatemynight@gmail.com>
2019-08-10 20:38:30 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -5,5 +5,20 @@ class Author
   def initialize(name)
     @name = name 
   end 
-
+  
+  def posts 
+    
+  end 
+  
+  def add_posts 
+    
+  end 
+  
+  def add_post_by_title 
+    
+  end 
+  
+  def self.post_count 
+    
+  end 
 end 
